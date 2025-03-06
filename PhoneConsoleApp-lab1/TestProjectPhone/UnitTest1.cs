01f7e74fbaeeaf7c5dac3b7e18655c7085e5e8f0
yaroslav.furmanov <yaroslav.furmanov@CH-01>
2025-03-06 11:29:50 +0100

    Test1

--- PhoneConsoleApp-lab1/TestProjectPhone/UnitTest1.cs
+++ PhoneConsoleApp-lab1/TestProjectPhone/UnitTest1.cs
@@ -1,11 +1,26 @@
+using ClassLibrary;
+
+
 namespace TestProjectPhone
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_konstruktor_dane_poprawne_Dzia³anie_ok()
         {
+            ///AAA
+
+
+            ///Arrange
+            var wlasciciel = "Furmanov";
+            var numerTelefonu = "123456789";
+            ///act
+            var phone = new Phone(wlasciciel, numerTelefonu);
+
+            ///assert
+            Assert.AreEqual(wlasciciel,phone.Owner);
+            Assert.AreEqual(numerTelefonu, phone.PhoneNumber);
         }
     }
 }
