5c28ea1d2dd2d3720b107d6c286e53228da95596
yaroslav.furmanov <yaroslav.furmanov@CH-01>
2025-03-13 10:16:27 +0100

    Gotowe testy

--- PhoneConsoleApp-lab1/TestProjectPhone/UnitTest1.cs
+++ PhoneConsoleApp-lab1/TestProjectPhone/UnitTest1.cs
@@ -19,8 +19,67 @@ namespace TestProjectPhone
             var phone = new Phone(wlasciciel, numerTelefonu);
 
             ///assert
-            Assert.AreEqual(wlasciciel,phone.Owner);
+            Assert.AreEqual(wlasciciel, phone.Owner);
             Assert.AreEqual(numerTelefonu, phone.PhoneNumber);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_konstruktor_pusty_wlasciciel_ArgumentException()
+        {
+            // Arrange
+            var wlasciciel = "";
+            var numerTelefonu = "123456789";
+
+            // Act
+            var phone = new Phone(wlasciciel, numerTelefonu);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_konstruktor_pusty_numerTelefonu_ArgumentException()
+        {
+            // Arrange
+            var wlasciciel = "Furmanov";
+            var numerTelefonu = "";
+
+            // Act
+            var phone = new Phone(wlasciciel, numerTelefonu);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_konstruktor_numerTelefonu_niepoprawna_dlugosc_ArgumentException()
+        {
+            // Arrange
+            var wlasciciel = "Furmanov";
+            var numerTelefonu = "12345";
+
+            // Act
+            var phone = new Phone(wlasciciel, numerTelefonu);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_konstruktor_null_wlasciciel_ArgumentException()
+        {
+            // Arrange
+            string wlasciciel = null;
+            var numerTelefonu = "123456789";
+
+            // Act
+            var phone = new Phone(wlasciciel, numerTelefonu);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_konstruktor_null_numerTelefonu_ArgumentException()
+        {
+            // Arrange
+            var wlasciciel = "Furmanov";
+            string numerTelefonu = null;
+
+            // Act
+            var phone = new Phone(wlasciciel, numerTelefonu);
         }
     }
 }
